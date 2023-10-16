f7c26136410f392f8ec092709b98ee84aeac04e8
ADARSH TIWARI <104240539+xd-Programmer007@users.noreply.github.com>
2023-10-16 14:04:17 +0530

    Added solution - LeetHub

--- Making A Large Island - GFG/making-a-large-island.cpp
+++ Making A Large Island - GFG/making-a-large-island.cpp
@@ -4,70 +4,67 @@ using namespace std;
 
 // } Driver Code Ends
 //User function Template for C++
+
 class Solution
 {
 public:
-    map<int,int> m;
-    vector<int> r  = {-1,0,1,0},c = {0,-1,0,1};
-    void bfs(vector<vector<int>>& grid,vector<vector<int>> &vis,int x,int y,int &count){
-        queue<pair<int,int>> q;
-        q.push({x,y});
-        grid[x][y] = count;
-        vis[x][y] = 1;
-        m[count]++;
-        while(!q.empty()){
-            auto temp = q.front();
-            q.pop();
-            int row=temp.first,col=temp.second;
-            for(int i=0;i<4;i++){
-                int nr = row+r[i],nc = col+c[i];
-                if(nr<grid.size() && nr>=0 && nc<grid[0].size() && nc>=0 && grid[nr][nc]==1 && vis[nr][nc]==0){
-                    q.push({nr,nc});
-                    grid[nr][nc] = count;
-                    m[count]++;
-                    vis[nr][nc] = 1;
-                }
+    
+    int dfs(int i, int j, int m, int n,int par, vector<vector<int>>& grid){
+        int dx[4] = {0,1,0,-1};
+        int dy[4] = {1,0,-1,0};
+        int cnt = 1;
+        for(int k = 0; k < 4; k++){
+            int x = i + dx[k], y = j + dy[k];
+            if((x>=0 && x < m && y>=0 && y < n) && grid[x][y] == 1){
+                grid[x][y] = par;
+                cnt += dfs(x, y, m, n ,par, grid);
             }
         }
-        count=count+1;
-        return;
+        return cnt;
     }
+    
     int largestIsland(vector<vector<int>>& grid) 
     {
-        int count = 2;
-        vector<vector<int>> vis(grid.size(),vector<int>(grid[0].size(),0));
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
-                if(grid[i][j]==1 && vis[i][j]==0){
-                    bfs(grid,vis,i,j,count);
+        // Your code goes here.
+        int m = grid.size(), n = grid[0].size();
+        vector<int> parent;
+        parent.resize(59999, 0);
+        int par = 2,ans = 1;
+        for(int i = 0; i < m ; i++){
+            for(int j = 0 ;j < n ; j++){
+                if(grid[i][j] == 1){
+                    grid[i][j] = par;
+                    parent[par] += dfs(i, j , m , n,par ,grid);
+                    ans = max(parent[par], ans);
+                    par++;
                 }
             }
         }
-        int ans = 0;
-        for(auto t:m){
-            ans = max(ans,t.second);
-        }
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
-                if(grid[i][j]==0){
-                    set<int> s;
-                    for(int k=0;k<4;k++){
-                        int nr = i+r[k],nc = j+c[k];
-                         if(nr<grid.size() && nr>=0 && nc<grid[0].size() && nc>=0 ){
-                             s.insert(grid[nr][nc]);
-                         }
+        
+        int dx[4] = {0,1,0,-1};
+        int dy[4] = {1,0,-1,0};
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == 0){
+                    int cnt = 1;
+                    unordered_set<int> st;
+                    for(int k = 0; k < 4; k++){
+                        int x = i + dx[k], y = j + dy[k];
+                        if((x>=0 && x < m && y>=0 && y < n) && grid[x][y] != 0 ){
+                            st.insert(grid[x][y]);
+                        }
                     }
-                    int t = 1;
-                    for(auto x:s){
-                        t+=m[x];
-                    }
-                    ans = max(ans,t);
+                    for(auto it : st)
+                        cnt += parent[it];
+                    ans = max(cnt, ans);
+                        
                 }
             }
         }
         return ans;
     }
 };
+
 
 
 //{ Driver Code Starts.
