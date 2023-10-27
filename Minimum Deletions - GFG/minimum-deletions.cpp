d0fd4e4b6c87d3acc8550a1c078f5e2faf0ce4c1
ADARSH TIWARI <104240539+xd-Programmer007@users.noreply.github.com>
2023-10-28 00:18:10 +0530

    Added solution - LeetHub

--- Minimum Deletions - GFG/minimum-deletions.cpp
+++ Minimum Deletions - GFG/minimum-deletions.cpp
@@ -8,26 +8,26 @@ using namespace std;
 
 class Solution{
   public:
-    int minimumNumberOfDeletions(string S) { 
+    using vvi = vector<vector<int>>;
+    
+    int minimumNumberOfDeletions(string s) { 
         // code here
-                int n = S.size();
-        vector<int> dp(n, 0);
-        for (int i = n - 1; i >= 0; i--) {
-            int prev = 0; 
-            for (int j = i; j < n; j++) {
-                int temp = dp[j]; 
-                if (i == j) {
-                    dp[j] = 1;
-                } else if (S[i] == S[j]) {
-                    dp[j] = 2 + prev;
-                } else {
-                    dp[j] = max(dp[j], dp[j - 1]);
-                }
-                prev = temp;
+        int n = s.length();
+        vvi dp( n , vector<int> (n , 0));
+        for(int i = 0; i < n; i++)
+            dp[i][i] = 1;
+            
+        for(int k = 1 ; k < n ; k++){
+            int i = 0, j = k;
+            while(i < n && j < n){
+                if(s[i] != s[j])
+                    dp[i][j] = max(dp[i+1][j], dp[i][j-1]);
+                else
+                    dp[i][j] = dp[i+1][j-1]+2;
+                i++, j++;
             }
         }
-
-        return n - dp[n - 1];
+        return n - dp[0][n-1];
     } 
 };
 
